d5d66071829834a2dcb90d1e50e1acee465560ec
paul <pau.hidalgov@duocuc.cl>
2024-08-28 20:35:45 -0400

    Creacion de publicacion y agregar foto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'practica3',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    Camera: {
+      android: {
+        // Permisos para Android
+        requestPermissions: true
+      }
+    }
+  }
 };
 
 export default config;
--- src/app/pages/publicar/publicar.page.ts
+++ src/app/pages/publicar/publicar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-publicar',
@@ -6,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publicar.page.scss'],
 })
 export class PublicarPage implements OnInit {
+  photoUrl: string = ''; // Inicializa photoUrl como cadena vacía
+  constructor(private toastController:ToastController,private router:Router) { }
+  public alertButtons = [
+    {
+      text: 'No',
+      cssClass: 'alert-button-cancel',
+    },
+    {
+      text: 'Si',
+      cssClass: 'alert-button-confirm',
+    },
+  ];
 
-  constructor() { }
+  async takePhoto() {
+    try {
+      const photo = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Photos
+      });
+  
+      if (photo.webPath) {
+        this.photoUrl = photo.webPath;
+      } else {
+        console.error('No se obtuvo una URL válida para la foto');
+      }
+  
+    } catch (error) {
+      console.error('Error al tomar la foto:', error);
+    }
+  }
+  clearPhoto() {
+    this.photoUrl = ''; // Restablece photoUrl para ocultar la imagen
+  }
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Publicado con exito',
+      duration: 1500,
+      position: position,
+      
+    });
+    this.router.navigate(['/menu']);
 
+    await toast.present();
+  }
+
+  
+  
   ngOnInit() {
   }
 

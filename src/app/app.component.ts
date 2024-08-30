464d3f10ede17910d73e455ad1dfe8bf68ecf6e2
paul <pau.hidalgov@duocuc.cl>
2024-08-29 23:12:03 -0400

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,25 @@ export class AppComponent {
   constructor(private router: Router) {  
     this.router.events.subscribe((event) => {
     if (event instanceof NavigationEnd) {
-      console.log('Navigated to:', event.url);  // Verifica la URL de navegación
+      console.log('Navigated to:', event.url);  
 
-      // Cambia 'tabs/home' al path de la pestaña donde deseas que el menú esté habilitado
+      
       if (event.url.includes('/menu')) {
-        this.isMenuDisabled = false; // Habilitar el menú
+        this.isMenuDisabled = false; 
+      }else if (event.url.includes('/tutores')) {
+        this.isMenuDisabled = false; 
+      }else if (event.url.includes('/perfil')) {
+        this.isMenuDisabled = false; 
+      }else if (event.url.includes('/nosotros')) {
+        this.isMenuDisabled = false; 
+      }else if (event.url.includes('/perfil')) {
+        this.isMenuDisabled = false; 
+      }else if (event.url.includes('/ayuda')) {
+        this.isMenuDisabled = false; 
+      }else if (event.url.includes('/asignaturas')) {
+        this.isMenuDisabled = false; 
       } else {
-        this.isMenuDisabled = true; // Deshabilitar el menú
+        this.isMenuDisabled = true; 
       }
     }
     
